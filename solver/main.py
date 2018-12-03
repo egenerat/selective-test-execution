5d9be719503a1da87a3e351832bdff0d5d53e1e4
egenerat <emile.generat@outlook.com>
2018-12-03 22:01:13 +0000

    Base for parser

--- solver/main.py
+++ solver/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import pprint
 
 def clean_line(line):
     return line.strip('\n').split(':')
@@ -8,9 +8,23 @@ FILENAME = sys.argv[1]
 F = open(FILENAME, "r")
 covered_files = []
 
+filename = ''
+covered_lines = {}
 for line in F.readlines():
     if line.startswith('SF'):
-        key, value = clean_line(line)
-        covered_files.append(value)
+        key, filename = clean_line(line)
+    if line.startswith('DA'):
+        key, coverage = clean_line(line)
+        line, hits = coverage.split(',')
+        covered_lines[line] = hits
+    if line == 'end_of_record\n':
+        covered_files.append({
+            "filename": filename,
+            "covered_lines": covered_lines
+        })
+        covered_lines = {}
 
-print(covered_files)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(covered_files)
+print('{} files covered'.format(len(covered_files)))
+# print(covered_files)
