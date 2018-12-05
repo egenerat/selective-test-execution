29d7668e74d20ced23f01905c33ae792e143c71d
egenerat <emile.generat@outlook.com>
2018-12-05 21:24:55 +0000

    Second test

--- js/test/test.js
+++ js/test/test.js
@@ -3,9 +3,17 @@ const a = require('../src/a.js');
 const b = require('../src/b.js');
 
 describe('a.js', () => {
-  describe('#sayHello()', () => {
+  describe('#getHello()', () => {
     it('should return "Hello"', () => {
       assert.equal(a.getHello(), "Hello");
+    });
+  });
+});
+
+describe('b.js', () => {
+  describe('#getResult()', () => {
+    it('should return 1', () => {
+      assert.equal(b.getResult(), 1);
     });
   });
 });
